1b02b1120aeb4b1d42a01dffc6ee27fbb6b33d68
ningwang-arch <60875240+ningwang-arch@users.noreply.github.com>
2020-05-14 15:10:45 +0800

    Random

--- hello.java
+++ hello.java
@@ -1,14 +1,8 @@
-import java.util.Scanner;
+import java.util.Random;
 public class hello{
 	public static void main(String[] args){
-		Scanner sc=new Scanner(System.in);
-		int num=sc.nextInt();
-		if(num%2==0)
-		{
-			System.out.println(num+"是偶数");
-		}else{
-			System.out.println(num+"是奇数");
-		}
-		sc.close();
+		Random r=new Random();
+		int num=r.nextInt(10);
+		System.out.println(num);
 	}	
 }
