3ffbf6f288ea645e5e513f172d926e4013bf2b48
ningwang-arch <60875240+ningwang-arch@users.noreply.github.com>
2021-01-26 16:26:50 +0800

    Update bi_an.py
    利用最近新找到的库对图片处理，进一步接近原图

--- code/spider/bi_an.py
+++ code/spider/bi_an.py
@@ -2,30 +2,25 @@ import requests
 from lxml import etree
 from PIL import Image
 import re
-# import re
+import os
+import pyanime4k
+import pathlib
 
-
-def create(path):  # 创建文件夹函数，输入路径
-    import os
-    path = path.strip()
-    path = path.rstrip('\\')
-    os.makedirs(path)
-
-
-#  create("D:\\testpic")
-Error = []  # 创建错误列表
+total_path='/your/picture/path/'
+if not os.path.exists(total_path):
+    os.makedirs(total_path)
+Error = []
 for i in range(1, 11):
     if (i == 1):
         list_url = 'http://pic.netbian.com/4kdongman/index.html'
     else:
         list_url = 'http://pic.netbian.com/4kdongman/index_{}.html'.format(i)
-    header = {
+    headers = {
         'Referer': 'http://pic.netbian.com/index.html',
         'User-Agent':
         'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4044.122 Safari/537.36',
-        'cookie': '__cfduid=d72b56fddfbff3f1b275c40c13184345c1588501223',
     }
-    list_response = requests.get(list_url, headers=header)
+    list_response = requests.get(list_url, headers=headers)
     list_html = list_response.content.decode('GBK')
     # print(list_html)
     element_html = etree.HTML(list_html)
@@ -48,14 +43,20 @@ for i in range(1, 11):
         # print(img_url)
         img = requests.get(img_url, headers=header)
         try:
-            path = "D:\\testpic\\{}.png".format(name[j])
+            path = total_path+("{}.png".format(name[j]))
             with open(path, 'wb') as f:
                 f.write(img.content)
-                img = Image.open(path)
+                # pyanime4k处理图片 提高分辨率
+                pyanime4k.upscale_images(pathlib.Path(path))
+                output_path=total_path+("{}_output.png".format(name[j]))
+                
+                # PIL修改回原像素，提高单位像素密度
+                img = Image.open(output_path)
                 img = img.resize((int(size[0]), int(size[1])),
                                  Image.ANTIALIAS)  # 改变大小 抗锯齿
                 # print(img.size)
                 img.save(path, quality=95)
+                os.remove(output_path)
                 print(name[j] + ' OK')
         except Exception:
             Error.append(name[j])
